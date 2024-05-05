c98be7201193e34a783ac2935c71c4ab92b76dc2
DevMaazKhan <maazokin@gmail.com>
2024-05-05 21:58:33 +0500

    complete100

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const products = [
+products = [
 	{
 		id: 001,
 		image: 'img/img/products/f1.jpg',
@@ -49,13 +49,76 @@ const products = [
 	},
 ];
 
+let userCart = [];
+
 const productsContainer = document.getElementById('products');
+const tableBody = document.getElementById('table-body');
+
+function onAddToCart(event) {
+	const productID =
+		event.target.parentElement.parentElement.getAttribute('data-id');
+
+	const product = products.find((p) => p.id === +productID);
+	const dublicate = userCart.find((p) => p.id === +productID);
+
+	if (dublicate) {
+		retun;
+	} else {
+		if (product) {
+			userCart.push(product);
+			console.log(userCart);
+			tableBody.innerHTML = '';
+
+			userCart.map((x) => {
+				const tr = document.createElement('tr');
+				tableBody.appendChild(tr);
+
+				const td1 = document.createElement('td');
+				tr.appendChild(td1);
+
+				const divtd1 = document.createElement('div');
+				divtd1.id = 'product-info';
+				td1.appendChild(divtd1);
+
+				const image = document.createElement('img');
+				image.src = x.image;
+				divtd1.appendChild(image);
+
+				const divtd2 = document.createElement('div');
+				divtd2.id = 'product-name';
+				divtd1.appendChild(divtd2);
+
+				const pTag = document.createElement('p');
+				pTag.innerHTML = x.name;
+				divtd2.appendChild(pTag);
+
+				const smallTag = document.createElement('small');
+				smallTag.innerHTML = x.price;
+				divtd2.appendChild(smallTag);
+
+				const td2 = document.createElement('td');
+				tr.appendChild(td2);
+
+				const input = document.createElement('input');
+				input.type = 'number';
+				input.value = 1;
+				td2.appendChild(input);
+
+				const td3 = document.createElement('td');
+				td3.innerHTML = x.price;
+				tr.appendChild(td3);
+			});
+		}
+	}
+}
 
 const generateShop = () => {
 	products.map((x) => {
 		let div1 = document.createElement('div');
 		div1.className = 'card';
 		productsContainer.appendChild(div1);
+		console.log('maazi', typeof x.id);
+		div1.setAttribute('data-id', x.id);
 
 		let img = document.createElement('img');
 		img.src = x.image;
@@ -77,16 +140,18 @@ const generateShop = () => {
 		let btn = document.createElement('button');
 		btn.innerHTML = 'Add to Cart';
 		btn.className = 'add-to-cart';
+
+		btn.addEventListener('click', onAddToCart);
 		div2.appendChild(btn);
 	});
 };
 
 generateShop();
 
-const addToCartBtn = document.querySelectorAll('.add-to-cart');
+// const addToCartBtn = document.querySelectorAll('.add-to-cart');
 
-addToCartBtn.forEach((button) => {
-	button.addEventListener('click', (e) => {
-		console.log('Hello Saad');
-	});
-});
+// addToCartBtn.forEach((button) => {
+// 	button.addEventListener('click', (e) => {
+// 		// console.log('Hello Saad');
+// 	});
+// });
